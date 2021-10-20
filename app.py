ce8ac820bc3a2d50cfc9049befc32d524d2c2d03
devpateI <dev.dp721@gmail.com>
2021-10-20 11:52:16 +0530

    loginWorking

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from flask_admin import Admin 
 from flask_admin.contrib.sqla import ModelView
@@ -6,18 +6,93 @@ import uuid
 from sqlalchemy_utils import EmailType, UUIDType
 import flask_admin as admin
 from flask_admin.contrib.sqla.filters import BaseSQLAFilter, FilterEqual
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mysecret'
+admin = Admin(app, name='Dev', template_mode='bootstrap3')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserLogin.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-admin = Admin(app, name='Dev', template_mode='bootstrap3')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
-app.config['SECRET_KEY'] = 'mysecret'
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
 
-db = SQLAlchemy(app)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('login')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit:
+        user=UserLogin.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password,form.password.data):
+                login_user(user)
+                return redirect(url_for("dashboard"))
+    return render_template("login.html", form=form)
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form=RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = UserLogin(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html",form=form)
+
+class UserLogin(db.Model,UserMixin):
+    id= db.Column(db.Integer,primary_key=True)
+    username= db.Column(db.String(20),nullable=False, unique=True)
+    password= db.Column(db.String(80),nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(),Length(min=4,max=20)],
+    render_kw={"placeholder": "Username"})
+
+    password = StringField(validators=[InputRequired(),Length(min=4,max=20)],
+    render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Register")
+
+    def validate_username(self,username):
+        existing_user_username = UserLogin.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please choose a different one")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(),Length(min=4,max=20)],
+    render_kw={"placeholder": "Username"})
+
+    password = StringField(validators=[InputRequired(),Length(min=4,max=20)],
+    render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
 
 class FilterLastNameBrown(BaseSQLAFilter):
     def apply(self, query, value, alias=None):
@@ -90,6 +165,7 @@ class UserAdmin(ModelView):
     ]
     
 admin.add_view(UserAdmin(User, db.session))
+admin.add_view(ModelView(UserLogin, db.session))
 class Mytools(ModelView):
     can_delete = False
     page_size = 50
